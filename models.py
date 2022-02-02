2833f42cbd8616e2cb0e317d7bb7d4a2f157013d
NicholasK <nicholask1@outlook.com>
2022-02-02 18:28:15 -0500

    Created the users table model

---
+++ models.py
@@ -0,0 +1,49 @@
+import bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+
+
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+def connect_db(app):
+    """Connects to the database"""
+
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    # id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), primary_key=True, unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(30), unique=True, nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+
+
+    @classmethod
+    def register(cls, username, password, email, first, last):
+        """Adds new user to database and encrypts their password"""
+
+        hashed = bcrypt.generate_password_hash(password)
+
+        hashed_utf8 = hashed.decode("utf8")
+
+        return cls(username=username, password=hashed_utf8, email=email, first_name=first, last_name=last)
+
+
+    @classmethod
+    def authenticate(cls, username, password):
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            return user
+        else:
+            return False
